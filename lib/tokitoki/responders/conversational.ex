3ad4db4e504f32e69286f8e17bce13ffdc27260d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-09-19 00:47:24 +0100

    add a bit of presentation for Tokitoki

--- lib/tokitoki/responders/conversational.ex
+++ lib/tokitoki/responders/conversational.ex
@@ -1,12 +1,19 @@
 defmodule Tokitoki.Responders.Conversational do
   use Hedwig.Responder
 
-  respond ~r/who are you/i, msg do
-    reply msg, """
-      I am Tokitoki, a bot! Every morning, I will reply to the
-      first person in the team with a different greeting
-      from around the world! 🌍
-    """
+  @presentation """
+  I am Tokitoki, a bot! Every morning, I will reply
+  to the first person in the team with a different greeting
+  from around the world! 🌍
+  """ |> String.replace("\n", " ")
+
+
+  respond ~r/(who|what) are you?/i, msg do
+    reply msg, @presentation
+  end
+
+  hear ~r/(who|what)('s| is) @?tokitoki/i, msg do
+    send msg, @presentation
   end
 end
 
